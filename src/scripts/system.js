5c2375f4f3731bea450c824e4ffe2bd8a26e00d6
Tampipi Neo <128296315+coldheart2002@users.noreply.github.com>
2023-11-18 19:03:04 +0800

    Add files via upload

--- src/scripts/system.js
+++ src/scripts/system.js
@@ -42,6 +42,20 @@ function system() {
     "Monitors, brightness, nightlight, display profile",
     "Volume levels, output, input, sound devices",
     "Alert from app and system, do not disturb",
+    "Reduce distractions",
+    "Sleep, battery usage, battery saver",
+    "Storage space, drives, configuration rules",
+    "Discoverability, received files location",
+    "Snap windows, desktops,task switching",
+    "These settings are intended for development use only",
+    "Activation state, subscriptions, product key",
+    "Recommended troubleshooters, preferences, history",
+    "Reset, advance startup, go back",
+    "Permissions, pairing PIN, discoverability",
+    "Remote Desktop users, connection permissions",
+    "Remove and manage system components that are preinstalled on Windows",
+    "Cut and copy history, sync, clear",
+    "About specifications, rename PC, specfications",
   ];
   let settingsMainContent = document.querySelectorAll(".settingsMainContent");
   let container = document.createElement("ul");
