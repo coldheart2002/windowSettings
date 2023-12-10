6dbafe6886bd1440bad846bc559a4d48adcc7e8d
Tampipi Neo <128296315+coldheart2002@users.noreply.github.com>
2023-12-10 18:09:02 +0800

    Add files via upload

---
+++ src/scripts/contentArrays.js
@@ -0,0 +1,188 @@
+let listContents = [
+  [],
+  [
+    //system
+
+    "Display",
+    "Sound",
+    "Notifications",
+    "Focus",
+    "Power & battery",
+    "Storage",
+    "Nearby sharing",
+    "Multitasking",
+    "For developers",
+    "Activation",
+    "Troubleshoot",
+    "Recovery",
+    "Projecting to this PC",
+    "Remote Desktop",
+    "System Components",
+    "Clipboard",
+    "About",
+  ],
+
+  [
+    //bluetooth
+
+    "Devices",
+    "Printers & scanners",
+    "Phone Link",
+    "Cameras",
+    "Mouse",
+    "Touchpad",
+    "Pen & Windows lnk",
+    "AutoPlay",
+    "USB",
+  ],
+
+  [
+    //network
+
+    "Wi-Fi",
+    "Ethernet",
+    "VPN",
+    "Mobile hotspot",
+    "Airplane mode",
+    "Proxy",
+    "Dial-up",
+    "Advance network settings",
+  ],
+
+  [
+    //personalization
+
+    "Background",
+    "Colors",
+    "Themes",
+    "Dynamic Lighting",
+    "Lock screen",
+    "Text input",
+    "Start",
+    "Taskbar",
+    "Fonts",
+    "Device usage",
+  ],
+
+  [
+    //apps
+
+    "Installed apps",
+    "Advanced app settings",
+    "Default apps",
+    "Offline maps",
+    "Optional features",
+    "Apps for websites",
+    "Video playback",
+    "Startup",
+  ],
+
+  [
+    //accounts
+
+    [
+      "Your info",
+      "Sign-in options",
+      "Email & accounts",
+      "Family",
+      "Windows backup",
+      "Access work or school",
+      "Passkey settings",
+    ],
+    [
+      "Account privacy",
+      "Subscriptions",
+      "Rewards",
+      "Payment options",
+      "Order history",
+      "Account & billing help",
+    ],
+  ],
+
+  [
+    //time
+
+    "Date & time",
+    "Language & region",
+    "Typing",
+    "Speech",
+  ],
+
+  [
+    //gaming
+
+    "Game Bar",
+    "Captures",
+    "Game Mode",
+  ],
+
+  [
+    //acceessibility
+
+    [
+      "Text size",
+      "Visual effects",
+      "Mouse pointer and touch",
+      "Text cursor",
+      "Magnifier",
+      "Color filters",
+      "Contrast themes",
+      "Narrator",
+      "For developers",
+      "Activation",
+    ],
+    ["Audio", "Captions"],
+    ["Speech", "Keyboard", "Mouse", "Eye control"],
+  ],
+
+  [
+    //privacy
+
+    ["Windows Security", "Find my device"],
+    [
+      "General",
+      "Speech",
+      "Inking & typing personalization",
+      "Diagnostics & feedback",
+      "Activity history",
+      "Seacrh permissions",
+      "Searching Windows",
+    ],
+    [
+      "Location",
+      "Camera",
+      "Microphone",
+      "Voice activation",
+      "Notifications",
+      "Account info",
+      "Contacts",
+      "Calendar",
+      "Phone calls",
+      "Call history",
+      "Email",
+      "Tasks",
+      "Messaging",
+      "Radios",
+      "Other devices",
+      "App diagnostics",
+      "Automatic file downloads",
+      "Documents",
+      "Downloads folder",
+      "Music library",
+      "Pictures",
+      "Videos",
+      "File system",
+      "Screenshot borders",
+      "Screenshots and apps",
+    ],
+  ],
+
+  [
+    //update
+
+    "Get the latest update as soon as possible",
+    "Pause updates",
+    "Update history",
+    "Advance options",
+  ],
+];
